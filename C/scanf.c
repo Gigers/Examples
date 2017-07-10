c41d801dbc7e920edbda1268eb5ad0b3204c8667
magaum <weslei_luiz22@hotmail.com>
2017-07-09 23:46:59 -0400

    scanf

---
+++ C/scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+    
+    int x;
+    
+    printf("Digite um valor: ");
+    scanf("%d",&x);
+    /*
+      & = endereço de alguma variável, neste caso o endereço de memória da variável x
+    */
+    printf("X = %d\n",x);
+      
+}
