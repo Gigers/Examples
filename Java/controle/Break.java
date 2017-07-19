b04c88bbd77847e141edea70744f96145840c7fb
Phelps <phelpsmemo@gmail.com>
2017-07-18 22:13:06 -0300

    Update Break.java

--- Java/controle/Break.java
+++ Java/controle/Break.java
@@ -1,11 +1,16 @@
 package Controle;
 
+/* 
+ O break é utilizado em loops, e serve para parar o loop.
+*/
+
 public class Break {
 	
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
 			if(i == 2) {
-				break;
+				break; //Veja que aqui o loop for será repetido até que o i seja igual a dois
+					//Quando i se igualar a 2 o laço será finalizado pelo break
 			}
 			System.out.println(i);
 		}
