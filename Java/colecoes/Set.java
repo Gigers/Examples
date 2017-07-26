cabc598bdc74246ad1153a4d85a141bc73a3a287
Felipe Menino <efelipecarlos@gmail.com>
2017-07-25 22:50:38 -0300

    Adição de coleções PT. 1

---
+++ Java/colecoes/Set.java
@@ -0,0 +1,13 @@
+package Colecoes;
+
+public class Set {
+	
+	//set
+	//Não é indexado
+	//pode ser ordenado, mas normalmente não é
+	//Pode ser homogenea ou heterogenea
+	//Não aceita objetos duplicados
+	
+	
+
+}
